f5691b38178b13c8bed4ba416c59fe97bd2e5d39
Luis <luisfelipe.sb@outlook.com>
2021-07-16 22:19:08 -0300

    Configurando typeorm, migrations e sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
-
-app.get('/', (_req, res) => {
-  return res.status(200).send({message: "Trying first route."});
-});
 
 app.listen(3000, () => {
   console.log('Server is running');
