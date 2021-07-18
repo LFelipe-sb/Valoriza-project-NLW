232f488bdbdb932477d99d561416d71624216ddd
Luis <luisfelipe.sb@outlook.com>
2021-07-18 20:39:35 -0300

    Tratamento de erros de forma assincrona

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import { router } from './routes';
 
 import 'reflect-metadata';
@@ -8,6 +9,17 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return res.status(400).json({error: err.message});
+  }
+
+  return res.status(500).json({
+    status: 'Error', 
+    message: `Internal Server Error: ${err}`
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server is running');
